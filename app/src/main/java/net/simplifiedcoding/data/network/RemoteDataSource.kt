9cbeb674a127cd52dbe3e69dad250f46bdc93f5c
chungisugithub <chungisu@gmail.com>
2022-12-11 20:09:57 +0900

    Passing Auth Token to Every API

--- app/src/main/java/net/simplifiedcoding/data/network/RemoteDataSource.kt
+++ app/src/main/java/net/simplifiedcoding/data/network/RemoteDataSource.kt
@@ -1,7 +1,10 @@
 package net.simplifiedcoding.data.network
 
 import android.content.Context
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import net.simplifiedcoding.BuildConfig
+import net.simplifiedcoding.data.UserPreferences
 import okhttp3.Authenticator
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -20,29 +23,35 @@ class RemoteDataSource @Inject constructor() {
         api: Class<Api>,
         context: Context
     ): Api {
-        val authenticator = TokenAuthenticator(context, buildTokenApi())
+        val authenticator = TokenAuthenticator(context, buildTokenApi(context))
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(getRetrofitClient(authenticator))
+            .client(getRetrofitClient(context, authenticator))
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(api)
     }
 
-    private fun buildTokenApi(): TokenRefreshApi {
+    private fun buildTokenApi(context: Context): TokenRefreshApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(getRetrofitClient())
+            .client(getRetrofitClient(context))
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(TokenRefreshApi::class.java)
     }
 
-    private fun getRetrofitClient(authenticator: Authenticator? = null): OkHttpClient {
+    private fun getRetrofitClient(context: Context,authenticator: Authenticator? = null): OkHttpClient {
+        val appContext = context.applicationContext
+        val userPreferences = UserPreferences(appContext)
+
+        val accessToken = runBlocking { userPreferences.accessToken.first() }
+
         return OkHttpClient.Builder()
             .addInterceptor { chain ->
                 chain.proceed(chain.request().newBuilder().also {
                     it.addHeader("Accept", "application/json")
+                    it.addHeader("Authorization", "Bearer $accessToken")
                 }.build())
             }.also { client ->
                 authenticator?.let { client.authenticator(it) }
