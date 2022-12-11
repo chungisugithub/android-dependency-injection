de2efbda029e09f0d5dbbae5c175200c21b3e943
chungisugithub <chungisu@gmail.com>
2022-12-12 04:35:18 +0900

    ready for dagger - clean MyApplication body 4 Dagger 2 Android Tutorial - Implementing Dagger Dependency Injection.mp4

--- app/src/main/java/net/simplifiedcoding/MyApplication.kt
+++ app/src/main/java/net/simplifiedcoding/MyApplication.kt
@@ -1,15 +1,7 @@
 package net.simplifiedcoding
 
 import android.app.Application
-import net.simplifiedcoding.di.AppContainer
 
 class MyApplication : Application() {
 
-    lateinit var appContainer: AppContainer
-
-    override fun onCreate() {
-        super.onCreate()
-
-        appContainer = AppContainer(this)
-    }
 }
