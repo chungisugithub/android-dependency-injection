758ec19ec4040f5bf04ea0dbdc74078b5bcb199d
chungisugithub <chungisu@gmail.com>
2022-12-12 05:38:17 +0900

    add DispatchingAndroidInjector member 4 Dagger 2 Android Tutorial - Implementing Dagger Dependency Injection.mp4

--- app/src/main/java/net/simplifiedcoding/MyApplication.kt
+++ app/src/main/java/net/simplifiedcoding/MyApplication.kt
@@ -2,9 +2,15 @@ package net.simplifiedcoding
 
 import android.app.Application
 import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
 class MyApplication : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var mInjector: DispatchingAndroidInjector<Any>
+
     override fun androidInjector(): AndroidInjector<Any> {
 
     }
