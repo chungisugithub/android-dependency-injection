12aaa2932e6d5c57d77ddc38a64c0284e54d9816
chungisugithub <chungisu@gmail.com>
2022-12-12 06:07:46 +0900

    edit contributeAuthActivityInjector 4 Dagger 2 Android Tutorial - Implementing Dagger Dependency Injection.mp4

--- app/src/main/java/net/simplifiedcoding/di/AuthActivityModule.kt
+++ app/src/main/java/net/simplifiedcoding/di/AuthActivityModule.kt
@@ -2,11 +2,12 @@ package net.simplifiedcoding.di
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import net.simplifiedcoding.ui.auth.AuthActivity
 import net.simplifiedcoding.ui.auth.LoginFragment
 
 @Module
 abstract class AuthActivityModule {
 
     @ContributesAndroidInjector
-    abstract fun contributeLoginFragmentInjector() : LoginFragment
+    abstract fun contributeAuthActivityInjector() : AuthActivity
 }
