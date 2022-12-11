9ae6b4b8e8e10d8794de3a1bc6c0745de124b5aa
chungisugithub <chungisu@gmail.com>
2022-12-12 06:58:01 +0900

    Summary - Steps 4 Dagger 2 Android Tutorial - Implementing Dagger Dependency Injection.mp4

--- app/src/main/java/net/simplifiedcoding/ui/auth/AuthActivity.kt
+++ app/src/main/java/net/simplifiedcoding/ui/auth/AuthActivity.kt
@@ -7,6 +7,13 @@ import net.simplifiedcoding.MyApplication
 import net.simplifiedcoding.R
 import javax.inject.Inject
 
+//#1 Add @Inject annotations to classes that you want to inject
+//#2 Create a module for the classes that cannot be constructor injected e.g. Interface
+//#3 Create Injectors for Activity/Fragments
+//#4 Create App Component
+//#5 Instantiate Dagger inside your application class, implement HasAndroidInjector and inject DispatchingAndroidInjector
+//#6 Use AndroidInjection.inject to inject dependencies with the help Inject annotation
+
 class AuthActivity : AppCompatActivity() {
 
     @Inject lateinit var viewModel: AuthViewModel
