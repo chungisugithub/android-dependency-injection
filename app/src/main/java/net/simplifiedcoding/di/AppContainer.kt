e8cbd8c724ecd28ec2dd6261ec2979889d42c384
chungisugithub <chungisu@gmail.com>
2022-12-12 02:48:06 +0900

    add context parameter to constructor of AppContainer 2 Android Dependency Injection - Manually Injecting Dependencies.mp4

--- app/src/main/java/net/simplifiedcoding/di/AppContainer.kt
+++ app/src/main/java/net/simplifiedcoding/di/AppContainer.kt
@@ -1,11 +1,12 @@
 package net.simplifiedcoding.di
 
+import android.content.Context
 import net.simplifiedcoding.data.UserPreferences
 import net.simplifiedcoding.data.network.AuthApi
 import net.simplifiedcoding.data.network.RemoteDataSource
 import net.simplifiedcoding.data.repository.AuthRepository
 
-class AppContainer {
+class AppContainer(context: Context) {
     val remoteDataSource = RemoteDataSource()
     val api = remoteDataSource.buildApi(AuthApi::class.java, requireContext())
     val preferences = UserPreferences(requireContext())
