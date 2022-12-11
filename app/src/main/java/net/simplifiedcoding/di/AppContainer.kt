1e2aa490e1610913e647b20a220577bf8c53336c
chungisugithub <chungisu@gmail.com>
2022-12-12 02:46:26 +0900

    cut dependents of LoginFragment and paste in AppContainer class 2 Android Dependency Injection - Manually Injecting Dependencies.mp4

--- app/src/main/java/net/simplifiedcoding/di/AppContainer.kt
+++ app/src/main/java/net/simplifiedcoding/di/AppContainer.kt
@@ -1,4 +1,13 @@
 package net.simplifiedcoding.di
 
+import net.simplifiedcoding.data.UserPreferences
+import net.simplifiedcoding.data.network.AuthApi
+import net.simplifiedcoding.data.network.RemoteDataSource
+import net.simplifiedcoding.data.repository.AuthRepository
+
 class AppContainer {
+    val remoteDataSource = RemoteDataSource()
+    val api = remoteDataSource.buildApi(AuthApi::class.java, requireContext())
+    val preferences = UserPreferences(requireContext())
+    val authRepository = AuthRepository(api, preferences)
 }
