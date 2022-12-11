a7e90c09959d34724c1f51b49fad12edf0a45f23
chungisugithub <chungisu@gmail.com>
2022-12-12 06:12:33 +0900

    inject viewModel to AuthActivity - AndroidInjection.inject(this) line, it must be inserted before super.onCreate() 4 Dagger 2 Android Tutorial - Implementing Dagger Dependency Injection.mp4

--- app/src/main/java/net/simplifiedcoding/ui/auth/AuthActivity.kt
+++ app/src/main/java/net/simplifiedcoding/ui/auth/AuthActivity.kt
@@ -2,12 +2,17 @@ package net.simplifiedcoding.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.android.AndroidInjection
 import net.simplifiedcoding.MyApplication
 import net.simplifiedcoding.R
+import javax.inject.Inject
 
 class AuthActivity : AppCompatActivity() {
 
+    @Inject lateinit var viewModel: AuthViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
     }
