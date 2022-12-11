cd568a3c5195b277adbb2f154d7a03ec18c6d070
chungisugithub <chungisu@gmail.com>
2022-12-12 05:55:27 +0900

    AndroidSupportInjection,inject(this) run app crash (unscoped) may not reference scoped bindings 4 Dagger 2 Android Tutorial - Implementing Dagger Dependency Injection.mp4

--- app/src/main/java/net/simplifiedcoding/ui/auth/LoginFragment.kt
+++ app/src/main/java/net/simplifiedcoding/ui/auth/LoginFragment.kt
@@ -5,18 +5,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.coroutines.launch
 import net.simplifiedcoding.R
-import net.simplifiedcoding.data.UserPreferences
-import net.simplifiedcoding.data.network.AuthApi
-import net.simplifiedcoding.data.network.RemoteDataSource
 import net.simplifiedcoding.data.network.Resource
-import net.simplifiedcoding.data.repository.AuthRepository
 import net.simplifiedcoding.databinding.FragmentLoginBinding
 import net.simplifiedcoding.ui.enable
 import net.simplifiedcoding.ui.handleApiError
@@ -33,7 +26,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        AndroidSupportInjection,inject(this)
+        AndroidSupportInjection.inject(this)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
