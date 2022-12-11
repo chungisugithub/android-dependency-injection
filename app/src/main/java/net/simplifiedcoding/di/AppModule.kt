5bd90a5c94f978bb6c1439e8bb2705b665cea292
chungisugithub <chungisu@gmail.com>
2022-12-12 05:13:23 +0900

    add fun provideAuthApi 4 Dagger 2 Android Tutorial - Implementing Dagger Dependency Injection.mp4

--- app/src/main/java/net/simplifiedcoding/di/AppModule.kt
+++ app/src/main/java/net/simplifiedcoding/di/AppModule.kt
@@ -1,9 +1,19 @@
 package net.simplifiedcoding.di
 
 import dagger.Module
+import dagger.Provides
+import net.simplifiedcoding.data.network.AuthApi
+import net.simplifiedcoding.data.network.RemoteDataSource
+import javax.inject.Singleton
 
 @Module
 class AppModule(
 
 ) {
+
+    @Singleton
+    @Provides
+    fun provideAuthApi(remoteDataSource: RemoteDataSource) {
+        remoteDataSource.buildApi(AuthApi::class.java,)
+    }
 }
