4fa8ebbaafa0bff851c9b12d0e84c11cd8989bf3
chungisugithub <chungisu@gmail.com>
2022-12-12 05:36:52 +0900

    implements androidInjector 4 Dagger 2 Android Tutorial - Implementing Dagger Dependency Injection.mp4

--- app/src/main/java/net/simplifiedcoding/MyApplication.kt
+++ app/src/main/java/net/simplifiedcoding/MyApplication.kt
@@ -1,8 +1,12 @@
 package net.simplifiedcoding
 
 import android.app.Application
+import dagger.android.AndroidInjector
 import dagger.android.HasAndroidInjector
 
 class MyApplication : Application(), HasAndroidInjector {
+    override fun androidInjector(): AndroidInjector<Any> {
+
+    }
 
 }
