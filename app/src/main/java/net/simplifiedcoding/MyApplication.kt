6f99570cc1fd4c2770b8c384b82937446d37862f
chungisugithub <chungisu@gmail.com>
2022-12-12 05:43:30 +0900

    add override fun onCreate() 4 Dagger 2 Android Tutorial - Implementing Dagger Dependency Injection.mp4

--- app/src/main/java/net/simplifiedcoding/MyApplication.kt
+++ app/src/main/java/net/simplifiedcoding/MyApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import net.simplifiedcoding.di.AppModule
+import net.simplifiedcoding.di.DaggerAppComponent
 import javax.inject.Inject
 
 class MyApplication : Application(), HasAndroidInjector {
@@ -11,8 +13,17 @@ class MyApplication : Application(), HasAndroidInjector {
     @Inject
     lateinit var mInjector: DispatchingAndroidInjector<Any>
 
-    override fun androidInjector(): AndroidInjector<Any> {
+    override fun onCreate() {
+        super.onCreate()
 
+        DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+            .inject(this)
+    }
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return mInjector
     }
 
 }
