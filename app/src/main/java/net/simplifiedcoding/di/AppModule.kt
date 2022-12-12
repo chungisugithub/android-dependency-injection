e6ee1172f8564e1be3c7f097ebcf9ebd24e0ba58
chungisugithub <chungisu@gmail.com>
2022-12-12 16:39:09 +0900

    provides context and remoteDataSource to provideAuthApi() 5 Android Hilt Tutorial - Injecting Dependencies with Hilt.mp4

--- app/src/main/java/net/simplifiedcoding/di/AppModule.kt
+++ app/src/main/java/net/simplifiedcoding/di/AppModule.kt
@@ -1,17 +1,20 @@
 package net.simplifiedcoding.di
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import net.simplifiedcoding.data.network.AuthApi
+import net.simplifiedcoding.data.network.RemoteDataSource
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
-     fun provideAuthApi() : AuthApi {
+     fun provideAuthApi(@ApplicationContext context: Context, remoteDataSource: RemoteDataSource) : AuthApi {
 
      }
 }
