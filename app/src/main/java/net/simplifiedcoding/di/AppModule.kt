33576e8d2b3d57bd9c36cd2b86c4771e7376b017
chungisugithub <chungisu@gmail.com>
2022-12-12 16:33:06 +0900

    create provide method for AuthApi as provideAuthApi() 5 Android Hilt Tutorial - Injecting Dependencies with Hilt.mp4

--- app/src/main/java/net/simplifiedcoding/di/AppModule.kt
+++ app/src/main/java/net/simplifiedcoding/di/AppModule.kt
@@ -1,10 +1,17 @@
 package net.simplifiedcoding.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import net.simplifiedcoding.data.network.AuthApi
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+     fun provideAuthApi() : AuthApi {
+
+     }
 }
